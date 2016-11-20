2804d162026f0468b727eb90d9237040955fe568
Shobhit Voleti <shobhitvoleti@gmail.com>
2016-11-20 21:36:42 +0530

    modified setup

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 import sys
 from setuptools import setup, find_packages
 
-requires = ["numpy","cv2","pypdfocr","Tkinter","glob","ttk","PIL"]
+requires = ["numpy","pypdfocr","Pillow","opencv-python"]
 
 setup(
         name="VisualScrollingAid",
         version="1.0",
         description="Scroll through PDF optically",
         url="https://github.com/shobhitvoleti/eye-tracking-scroller",
-        author="Shobhit Voleti","Siddharth Nair"
-        author_email="shobhitvoleti@gmail.com, siddharth.nair@iitb.ac.in",
+        author="Shobhit Voleti, Siddharth Nair",
+        author_email="shobhitvoleti@gmail.com,siddharth.nair@iitb.ac.in",
         license="GPL3",
         install_requires=requires,
         packages=find_packages(),
