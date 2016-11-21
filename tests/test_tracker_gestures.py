83b3cc8f5283275dcd291592bf8b20e08f80299a
Siddharth Nair <siddharth.nair@iitb.ac.in>
2016-11-21 08:55:21 +0530

    fixed tests

--- tests/test_tracker_gestures.py
+++ tests/test_tracker_gestures.py
@@ -30,13 +30,13 @@ class TestsNodandScroll(unittest.TestCase):
 
     def test_check_if_correct_directive_for_staying(self):
         directive1=ns.scroll_pdf(250,[220,180,260,270,290,275])
-        self.assertEqual1(directive1,0)
+        self.assertEqual(directive1,0)
         directive2=ns.scroll_pdf(250,[220,240,260,270,200])
-        self.assertEqual1(directive2,0)
+        self.assertEqual(directive2,0)
         directive3=ns.scroll_pdf(250,[490,400,460,320,290])
-        self.assertEqual1(directive3,0)
+        self.assertEqual(directive3,0)
         directive4=ns.scroll_pdf(250,[80,160,180,200,240])
-        self.assertEqual1(directive4,0)
+        self.assertEqual(directive4,0)
 
     def test_check_if_correct_directive_for_scrollingdown(self):
         directive=ns.scroll_pdf(250,[260,200,160,110,100,75])
@@ -48,13 +48,13 @@ class TestsNodandScroll(unittest.TestCase):
 
     def test_check_if_correct_directive_for_staying_on_current_page(self):
         directive1=ns.next_page(250,[220,180,260,270,290,275])
-        self.assertEqual1(directive1,0)
+        self.assertEqual(directive1,0)
         directive2=ns.next_page(250,[220,240,260,270,200])
-        self.assertEqual1(directive2,0)
+        self.assertEqual(directive2,0)
         directive3=ns.next_page(250,[490,400,460,320,290])
-        self.assertEqual1(directive3,0)
+        self.assertEqual(directive3,0)
         directive4=ns.next_page(250,[80,160,180,200,240])
-        self.assertEqual1(directive4,0)
+        self.assertEqual(directive4,0)
 
     def test_check_if_correct_directive_for_turning_to_previous_page(self):
         directive=ns.next_page(250,[260,200,160,110,100,75])
